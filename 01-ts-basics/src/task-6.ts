65d81d1d9a037584e8fbad2489d4b3d31dfdb8b8
Viktoria Zharina <159477668+ViktoriaZharina@users.noreply.github.com>
2025-05-09 12:28:16 +0300

    chfgfh

--- 01-ts-basics/src/task-6.ts
+++ 01-ts-basics/src/task-6.ts
@@ -1,9 +1,7 @@
-function getFirstElement<T extends string | number | boolean>(arr: T[]): T {
+function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
 getFirstElement([1, 2, 3]); // 1
 getFirstElement(["a", "b", "c"]); // "a"
 getFirstElement([true, false, true]); // true
-
-getFirstElement(["rtrt", 4, "ryry"]);
